3d31e7261512dac78bd952b47bfbac51ad1459ce
Diego Fuentes <DiegoFu@MacBook-Air-de-Diego.local>
2016-07-05 13:28:18 -0500

    nuevo js para filtros

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,9 +1,13 @@
 //Funcion de paginacion para la parte de portafolio.html
 
 $(document).on("ready",function(){
+  //Inicializar paginacion (tabs).
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
 
   var counter = 0;
-
+  var nameCategory;
   $(document).on("click", ".go-right", function(){
 
     var lengthSelected =  $('.wrapper div.content-tab').length-1;
@@ -41,6 +45,7 @@ $(document).on("ready",function(){
 
     return false;
   });
+
   $(document).on("click", "a.number", function(){
 
     counter = $(this).attr("name");
@@ -56,52 +61,16 @@ $(document).on("ready",function(){
     return false;
   });
 
-  $(document).ready(function() {
-  //Inicializar paginacion (tabs).
-      $(function() {
-        $( "#tabs" ).tabs();
-      });
-  //Opcion del menu selecionada
   $(document).on("click", '.opt-list', function(){
-      $('.opt-list').removeClass('selected');
-      $(this).addClass('selected');
-    });
-  //Filtrar a todos
-  $(document).on("click", ".opt-todos", function(){
-    $("li[name=interiorismo],li[name=habitacional],li[name=comercial],li[name=design]").removeClass('noshow-item');
-    $("li[name=interiorismo],li[name=habitacional],li[name=comercial],li[name=design]").addClass('show-item');
-    });
-  //Filtrar solo diseño
-  $(document).on("click", ".opt-design", function(){
-    $("li[name=interiorismo],li[name=habitacional],li[name=comercial],li[name=design]").removeClass('show-item');
-    $("li[name=interiorismo],li[name=habitacional],li[name=comercial],li[name=design]").addClass('noshow-item');
-    setTimeout(function(){
-      $("li[name=design]").removeClass('noshow-item');
-    }, 10)
-    $("li[name=design]").addClass('show-item');
+    $('.opt-list').removeClass('selected');
+    $(this).addClass('selected');
+    $("ul#Grid li").removeClass('noshow-item');
+    $("ul#Grid li").addClass('show-item');
+    nameCategory = $(this).attr('data-name');
+    $("ul#Grid li").removeClass('show-item');
+    $("ul#Grid li").addClass('noshow-item');
+    $("ul#Grid li."+nameCategory).removeClass('noshow-item');
+    $("ul#Grid li."+nameCategory).addClass('show-item');
   });
-  //Filtrar solo interiorismo
-  $(document).on("click", ".opt-int", function(){
-      $("li[name=design],li[name=habitacional],li[name=comercial],li[name=interiorismo]").removeClass('show-item');
-      $("li[name=design],li[name=habitacional],li[name=comercial],li[name=interiorismo]").addClass('noshow-item');
-      $("li[name=interiorismo]").removeClass('noshow-item');
-      $("li[name=interiorismo]").addClass('show-item');
-    });
-  //Filtrar solo habitacional
-  $(document).on("click", ".opt-hab", function(){
-      $("li[name=design],li[name=interiorismo],li[name=comercial]").removeClass('show-item');
-      $("li[name=design],li[name=interiorismo],li[name=comercial]").addClass('noshow-item');
-      $("li[name=habitacional]").removeClass('noshow-item');
-      $("li[name=habitacional]").addClass('show-item');
-    });
-  //Filtrar solo comercial
-  $(document).on("click", ".opt-com", function(){
-      $("li[name=design],li[name=interiorismo],li[name=habitacional]").removeClass('show-item');
-      $("li[name=design],li[name=interiorismo],li[name=habitacional]").addClass('noshow-item');
-      $("li[name=comercial]").removeClass('noshow-item');
-      $("li[name=comercial]").addClass('show-item');
-    });
-  });
-
 
 });
